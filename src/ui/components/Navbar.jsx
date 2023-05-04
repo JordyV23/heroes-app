2e72afeecc72f3ee87e5283142bc662c1a471470
JordyV23 <jordyvargas0123@gmail.com>
2023-05-03 18:49:58 -0600

    uso de navigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const onLogout = ()  => {
+    navigate('/login',{
+      replace:true
+    })
+  }
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -36,7 +45,7 @@ export const Navbar = () => {
         <ul className="navbar-nav ml-auto">
          
          <span className="nav-item nav-link text-primary"> Jordy </span>
-          <button className="nav-item nav-link btn">
+          <button className="nav-item nav-link btn" onClick={onLogout}>
             Logout
           </button>
         </ul>
